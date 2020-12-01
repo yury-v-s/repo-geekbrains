3e7aa0f83d5b589e353de86da4799e628a94ee67
Yury S <report_spam@inbox.ru>
2020-12-01 19:23:20 +0300

    Добавлены задания к пятому уроку

---
+++ Lesson_5.py
@@ -0,0 +1,16 @@
+# Task 1
+
+
+with open('Lesson_5_1.txt', 'w', encoding='utf-8') as doc:
+    while True:
+        line = input('Введите информацию для записи, либо пусто для завершения: ')
+        if not line:
+            break
+        print(line, file=doc)
+
+# Task 2
+# Task 3
+# Task 4
+# Task 5
+# Task 6
+# Task 7
